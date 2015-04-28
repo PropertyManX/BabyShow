f251f0611960fb989b58b7b8f7351c729e900485
DeleteThePast <blackmambaX@163.com>
2015-04-28 16:24:12 +0800

    First text

---
+++ BabyShow/BabyShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabyShow
+//
+//  Created by DeleteThePast on 15/4/28.
+//  Copyright (c) 2015年 babyShow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BabyShow/BabyShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BabyShow
+//
+//  Created by DeleteThePast on 15/4/28.
+//  Copyright (c) 2015年 babyShow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
