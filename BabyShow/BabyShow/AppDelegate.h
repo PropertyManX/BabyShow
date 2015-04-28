c751ff34627aff983ab08ad3a23965752784c134
DeleteThePast <blackmambaX@163.com>
2015-04-28 17:07:12 +0800

    no message

---
+++ BabyShow/BabyShow/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  BabyShow
+//
+//  Created by DeleteThePast on 15/4/28.
+//  Copyright (c) 2015年 babyShow. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
